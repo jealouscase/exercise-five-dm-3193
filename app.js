4724f304a3ebb5a9127a45873fe3e21c37d67cff
jealouscase <henryosterweis@gmail.com>
2024-11-05 12:26:18 -0500

    fix

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const app = express()
 const port = 3000
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyAkwIEuZEf8N3cHJSp3IlqODBY7DMP8IZY",
-    authDomain: "exercise-five-90fab.firebaseapp.com",
-    projectId: "exercise-five-90fab",
-    storageBucket: "exercise-five-90fab.firebasestorage.app",
-    messagingSenderId: "46563974192",
-    appId: "1:46563974192:web:524d84b750f957991ce1f5"
-}
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAkwIEuZEf8N3cHJSp3IlqODBY7DMP8IZY",
+//     authDomain: "exercise-five-90fab.firebaseapp.com",
+//     projectId: "exercise-five-90fab",
+//     storageBucket: "exercise-five-90fab.firebasestorage.app",
+//     messagingSenderId: "46563974192",
+//     appId: "1:46563974192:web:524d84b750f957991ce1f5"
+// }
 
 firebase.initializeApp(firebaseConfig)
 
